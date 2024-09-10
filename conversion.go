130ddc34e89f1f63c147edbb3c468688bdb51041
Anurag <anurag@xrpscan.com>
2024-09-10 19:39:22 +0530

    Fixes #7 - Add ripple time to unix time conversion functions

---
+++ conversion.go
@@ -0,0 +1,39 @@
+package xrpl
+
+import (
+	"time"
+)
+
+/*
+ * Ripple time to Unix time conversion
+ */
+
+// Seconds since UNIX Epoch to Ripple Epoch (2000-01-01T00:00 UTC)
+// https://xrpl.org/docs/references/protocol/data-types/basic-data-types#specifying-time
+const RIPPLE_EPOCH_DIFF int64 = 946684800
+
+// Convert a Ripple timestamp to a unix timestamp.
+func RippleTimeToUnixTime(rippleTime int64) int64 {
+	return (rippleTime + RIPPLE_EPOCH_DIFF)
+}
+
+// Convert a unix timestamp to a Ripple timestamp.
+func UnixTimeToRippleTime(unixTime int64) int64 {
+	return (unixTime - RIPPLE_EPOCH_DIFF)
+}
+
+// Convert a Ripple timestamp to an ISO8601 time.
+func RippleTimeToISOTime(rippleTime int64) string {
+	unixTime := RippleTimeToUnixTime(rippleTime)
+	return time.Unix(unixTime, 0).Format(time.RFC3339)
+}
+
+// Convert an ISO8601 timestamp to a Ripple timestamp.
+func IsoTimeToRippleTime(isoTime string) (int64, error) {
+	theTime, err := time.Parse(time.RFC3339, isoTime)
+	if err != nil {
+		return 0, err
+	}
+	rippleTime := UnixTimeToRippleTime(theTime.Unix())
+	return rippleTime, nil
+}
